42a12e07f8d4c0a30a3d2b68516255837a96e40d
Starford Omwakwe <starfordomwakwe@gmail.com>
2020-11-21 23:02:07 +0300

    add the auth api in it own app

--- anothertodo/settings.py
+++ anothertodo/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "todoapp",
     "rest_framework",
     "corsheaders",
+    "authentication",
 ]
 
 MIDDLEWARE = [
--- anothertodo/urls.py
+++ anothertodo/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-# drf stuff
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
 
 # simplejwt imports
 from rest_framework_simplejwt.views import (
@@ -27,26 +24,10 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ["url", "username", "email", "is_staff"]
-
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r"users", UserViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include(router.urls)),
+    path("api/", include("authentication.urls")),
     path("api/", include("todoapp.urls")),
     # path("", include("todoapp.urls")),
     path("api-auth/", include("rest_framework.urls")),
