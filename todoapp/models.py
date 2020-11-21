54ad11b90af82209c26be4596548e8d9a0e2284d
Starford Omwakwe <starfordomwakwe@gmail.com>
2020-11-21 22:14:04 +0300

    add the todo model

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Todo(models.Model):
+    """
+    The todo model
+    """
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    done = models.BooleanField(default=False)
+    date_added = models.DateField(auto_now_add=True)
+    date_finished = models.DateField(auto_now=True, null=True)
+
+    def __str__(self):
+        """
+        object representation
+        """
+        return self.title
